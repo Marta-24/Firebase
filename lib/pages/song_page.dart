a0f8ddae4733f91b94e123998a22a96a4c6215b3
Marta <73582921+Marta-24@users.noreply.github.com>
2023-12-04 20:06:38 +0100

    Song Page better design
    the yellow is the cover, the grey where the lyrics are going to be

--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -1,34 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/widgets/bottom_bar.dart';
 
 class SongPage extends StatelessWidget {
   final String title;
   final String artist;
 
-  const SongPage({Key? key, required this.title, required this.artist})
-      : super(key: key);
+  const SongPage({
+    Key? key,
+    required this.title,
+    required this.artist,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Song Details'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Title: $title',
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Artist: $artist',
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
+        backgroundColor: const Color.fromARGB(255, 29, 34, 73),
+        title: const Text(
+          'Now Playing',
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
       ),
+      backgroundColor: const Color.fromARGB(255, 29, 34, 73),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 200.0,
+                width: 200.0,
+                color: Colors.yellow,
+              ),
+              const SizedBox(height: 16.0),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                artist,
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.menu, color: Colors.white),
+                  Icon(Icons.menu, color: Colors.white),
+                  Icon(Icons.menu, color: Colors.white),
+                  Icon(Icons.menu, color: Colors.white),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // LEFT LINE
+                  Container(
+                    height: 10.0,
+                    width: 150.0,
+                    color: Colors.red,
+                  ),
+                  // CIRCLE
+                  Container(
+                    height: 25.0,
+                    width: 25.0,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white, 
+                    ),
+                  ),
+                  // RIGHT LINE
+                  Container(
+                    height: 10.0,
+                    width: 200.0,
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomBar(
+  currentIndex: 3, // Assuming 3 is the index for 'My Songs'
+  onTap: (index) {
+    switch (index) {
+      case 0:
+        Navigator.of(context).pushReplacementNamed('/');
+        break;
+      case 1:
+        Navigator.of(context).pushReplacementNamed('/search');
+        break;
+      case 2:
+        Navigator.of(context).pushReplacementNamed('/playlist');
+        break;
+      case 3:
+        // No need to navigate to the same page, but you can handle it differently if needed
+        break;
+      case 4:
+        Navigator.of(context).pushReplacementNamed('/more');
+        break;
+    }
+  },
+),
     );
   }
 }
